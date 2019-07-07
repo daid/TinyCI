be9b49d50721d8f34eba0dbe97dac762de6855b3
Daid <daid303@gmail.com>
2019-07-07 19:30:27 +0000

    Update tags from remote before fetching sources

--- src/git.py
+++ src/git.py
@@ -9,4 +9,5 @@ def checkout(repos, sha, source_path):
         run.cmd(["git", "clone", "https://github.com/%s.git" % (repos), os.path.basename(source_path)], cwd=base_path)
     else:
         run.cmd(["git", "fetch"], cwd=source_path)
+        run.cmd(["git", "fetch", "--tags"], cwd=source_path)
     run.cmd(["git", "checkout", sha], cwd=source_path)
